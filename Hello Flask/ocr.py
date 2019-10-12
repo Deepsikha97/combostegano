909a48db4a5b24ae0933c62aab629bfaa4e51d17
Kinjal Das <kinjal.das86@gmail.com>
2019-10-12 16:18:47 +0530

    OCR working

--- Hello Flask/ocr.py
+++ Hello Flask/ocr.py
@@ -1,26 +1,13 @@
-from google.cloud import vision
-import io
+from PIL import Image
+import pytesseract
 
-def detect_text(path="./images/decoded_image.png"):
-    """Detects text in the file."""
-    print(path)
-    client = vision.ImageAnnotatorClient()
-    print(client)
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-    print(content)
-    image = vision.types.Image(content=content)
-    print(image)
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    print('Texts:')
 
-    for text in texts:
-        print('\n"{}"'.format(text.description))
+def ocr_core(filename="./images/decoded_image.png"):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    pytesseract.pytesseract.tesseract_cmd = './OCR/tesseract.exe'
+    text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text
 
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
-
-        print('bounds: {}'.format(','.join(vertices)))
-
-detect_text()
+print(ocr_core())
